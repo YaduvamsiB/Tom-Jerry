08d6eb0d77d6c61cece8f35e22a069a2fea5f03a
YaduvamsiB <76206851+YaduvamsiB@users.noreply.github.com>
2021-01-27 17:17:55 +0530

    added Images

--- sketch.js
+++ sketch.js
@@ -22,14 +22,15 @@ function setup() {
   createCanvas(600, 500);
   //create tom and jerry sprites here
 
-  tom = createSprite(400, 400);
+  tom = createSprite(400, 400, 50, 50);
   tom.addAnimation("tomImage", tomImage);
-  tom.addAnimation("tomStop", tomImage3);
+  //tom.addAnimation("tomStop", tomImage3);
+  //tom.addAnimation("tomRunning", tomImage2);
   tom.scale = 0.1;
 
-  jerry = createSprite(40, 400);
+  jerry = createSprite(40, 400, 50, 50);
   jerry.addAnimation("jerryImage", jerryImage);
-  jerry.addAnimation("jerryStop", jerryImage2);
+  //jerry.addAnimation("jerryStop", jerryImage2);
 
   jerry.scale = 0.1;
 }
@@ -40,13 +41,27 @@ function draw() {
 
   keyPressed();
 
-  if (tom.isTouching(jerry)) {
+  console.log(isTouch(tom, jerry));
+  if (isTouch(tom, jerry)) {
     tom.changeAnimation("tomStop");
     jerry.changeAnimation("jerryStop");
+    console.log("touched!");
+    tom.scale = 0.1;
+    jerry.scale = 0.1;
     tom.velocityX = 0;
+  } else {
+    tom.changeAnimation("tomRunning");
+    jerry.changeAnimation("jerryImage");
+    console.log("nope!");
+    //tom.velocityX = 0;
   }
 
-  //tom.debug();
+  tom.debug = true;
+  tom.setCollider("circle", 0, 0, 500);
+
+  jerry.debug = true;
+  jerry.setCollider("circle", 0, 0, 500);
+
   drawSprites();
 }
 
@@ -54,8 +69,7 @@ function keyPressed() {
   //For moving and changing animation write code here
 
   if (keyDown("LEFT_ARROW")) {
-    tom.velocityX = -3;
-    tom.addAnimation("tomRunning", tomImage2);
+    tom.velocityX = -30;
     tom.changeAnimation("tomRunning");
   }
 }
